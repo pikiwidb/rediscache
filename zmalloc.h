af27e9c0fe21ecf4af7f35ad40b272cc62905e19
chejinge <chejinge@360.cn>
2023-10-19 12:44:09 +0800

    fix_centos_build

--- zmalloc.h
+++ zmalloc.h
@@ -32,8 +32,8 @@
 #define __ZMALLOC_H
 
 /* Double expansion needed for stringification of macro values. */
-#define __xstr(s) __str(s)
-#define __str(s) #s
+#define __xstr(s) __xsstr(s)
+#define __xsstr(s) #s
 
 #if defined(USE_TCMALLOC)
 #define ZMALLOC_LIB ("tcmalloc-" __xstr(TC_VERSION_MAJOR) "." __xstr(TC_VERSION_MINOR))
